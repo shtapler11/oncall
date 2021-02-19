c4fe66646af7527a2d9ba361a53ec9fc1fd96f31
sunni <shtapler11@gmail.com>
2021-02-19 14:54:07 +0000

    OC-3 return for today's user

--- app/controllers/OnCallController.scala
+++ app/controllers/OnCallController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import models.{Date, UserOnCall}
+
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -13,7 +15,18 @@ class OnCallController @Inject()(val controllerComponents: ControllerComponents)
 
   def getDateInfo(dates: List[String]): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     dates match {
-      case Nil => Ok("No date")
+      case Nil =>
+        val today: String = Date.today.date
+
+        // Find an on-call user on this date
+        val result: Option[UserOnCall] = UserOnCall.usersOnCall.find(userOnCall =>
+          userOnCall.date.date.contentEquals(today))
+
+        result match {
+          case Some(value) => Ok(value.toString)
+          case None => Ok("No user allocated")
+        }
+
       case _ => Ok(dates.mkString(","))
     }
   }
