ed2d82caed85ad023727f2b529ff7544dcbcbba0
sunni <shtapler11@gmail.com>
2021-02-19 13:31:25 +0000

    OC-3 first endpoint

--- app/controllers/OnCallController.scala
+++ app/controllers/OnCallController.scala
@@ -7,7 +7,5 @@ import play.api.mvc._
 @Singleton
 class OnCallController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+  def getName: Action[AnyContent] = Action { Ok("Hi there") }
 }
