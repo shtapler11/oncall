39743a5fae49a15cad0cea955e7c747e085c9280
sunni <shtapler11@gmail.com>
2021-02-19 15:02:37 +0000

    OC-3 created getOnCallUserFromDate

--- app/controllers/OnCallController.scala
+++ app/controllers/OnCallController.scala
@@ -13,19 +13,20 @@ class OnCallController @Inject()(val controllerComponents: ControllerComponents)
 
   def getDate(dateId: String): Action[AnyContent] = Action { Ok(s"Hi there $dateId") }
 
+  def getOnCallUserFromDate(date: String) = {
+    val result: Option[UserOnCall] = UserOnCall.usersOnCall.find(userOnCall => userOnCall.date.date.contentEquals(date))
+    result match {
+      case Some(value) => Ok(value.toString)
+      case None => Ok("No user allocated")
+    }
+  }
+
   def getDateInfo(dates: List[String]): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     dates match {
       case Nil =>
         val today: String = Date.today.date
-
         // Find an on-call user on this date
-        val result: Option[UserOnCall] = UserOnCall.usersOnCall.find(userOnCall =>
-          userOnCall.date.date.contentEquals(today))
-
-        result match {
-          case Some(value) => Ok(value.toString)
-          case None => Ok("No user allocated")
-        }
+        getOnCallUserFromDate(today)
 
       case _ => Ok(dates.mkString(","))
     }
